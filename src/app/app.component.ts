6e5ad4909293aac7898247331589e9524dd320ed
EkremKanuric <ekei.kanuric@gmail.com>
2024-02-12 18:37:02 +0100

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolBarComponent } from './tool-bar/tool-bar.component'; 
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToolBarComponent],
+  imports: [RouterOutlet,ToolBarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
